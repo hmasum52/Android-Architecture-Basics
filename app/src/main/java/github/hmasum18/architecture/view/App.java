fc61bf020d27897d664ede40e580a1d763d881dd
Hasan masum <masumbuetcse18@gmail.com>
2021-09-06 14:45:24 +0600

    wrapped the application with App

--- app/src/main/java/github/hmasum18/architecture/view/App.java
+++ app/src/main/java/github/hmasum18/architecture/view/App.java
@@ -1,4 +1,10 @@
 package github.hmasum18.architecture.view;
 
-public class App {
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
